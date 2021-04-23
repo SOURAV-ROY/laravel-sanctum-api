cfdda1567e8494ef7e49d34f5f036c47f37d2055
SOURAV ROY <souravroy.info13@gmail.com>
2021-04-23 15:38:28 +0600

    ProductController Control Everything index/store/show

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Database\Eloquent\Collection|Product[]
+     */
+    public function index()
+    {
+        return Product::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+//        return Product::create([
+//            'name' => 'My Product',
+//            'slug' => 'my-product',
+//            'description' => 'My Product Description',
+//            'price' => '99.99',
+//        ]);
+
+        $request->validate([
+            'name' => 'required',
+            'slug' => 'required',
+            'description' => 'required',
+            'price' => 'required',
+        ]);
+
+        return Product::create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return Product::find($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -15,18 +16,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/products', function () {
-    return Product::all();
-});
+Route::resource('/products', ProductController::class);
 
-Route::post('/products', function () {
-    return Product::create([
-        'name' => 'My Product',
-        'slug' => 'my-product',
-        'description' => 'My Product Description',
-        'price' => '99.99',
-    ]);
-});
+//Route::get('/products', [ProductController::class, 'index']);
+//Route::post('/products', [ProductController::class, 'store']);
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
