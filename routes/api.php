17c69f9c14e90b2583b96784ddca5eee0fafab74
SOURAV ROY <souravroy.info13@gmail.com>
2021-04-23 15:10:43 +0600

    Create Model-Migration GET/POST Data In API

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,7 +16,16 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/products', function () {
-    return "Products";
+    return Product::all();
+});
+
+Route::post('/products', function () {
+    return Product::create([
+        'name' => 'My Product',
+        'slug' => 'my-product',
+        'description' => 'My Product Description',
+        'price' => '99.99',
+    ]);
 });
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
