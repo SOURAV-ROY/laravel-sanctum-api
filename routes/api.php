f2f7af99263a9f25cdfcf12dd0251f1d4819fe1c
SOURAV ROY <souravroy.info13@gmail.com>
2021-04-24 00:51:58 +0600

    API Route Update

--- routes/api.php
+++ routes/api.php
@@ -17,16 +17,20 @@ use Illuminate\Support\Facades\Route;
 */
 
 //Public Routes
-Route::resource('/products', ProductController::class);
-//Route::get('products/search/{name}', [ProductController::class, 'search']);
+//Route::resource('/products', ProductController::class);
+Route::get('/products', [ProductController::class, 'index']);
+Route::get('/products/{id}', [ProductController::class, 'show']);
+Route::get('products/search/{name}', [ProductController::class, 'search']);
 
 //Protected Routes
 Route::group(['middleware' => ['auth:sanctum']], function () {
-    Route::get('products/search/{name}', [ProductController::class, 'search']);
+//    Route::get('products/search/{name}', [ProductController::class, 'search']);
+    Route::post('/products', [ProductController::class, 'store']);
+    Route::put('/products/{id}', [ProductController::class, 'update']);
+    Route::delete('/products/{id}', [ProductController::class, 'destroy']);
+
 });
 
-//Route::get('/products', [ProductController::class, 'index']);
-//Route::post('/products', [ProductController::class, 'store']);
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
