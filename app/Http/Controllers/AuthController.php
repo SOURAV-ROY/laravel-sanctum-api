e2b57ea6745ba6091e2f13832d61d68e66763ec9
SOURAV ROY <souravroy.info13@gmail.com>
2021-04-24 01:53:43 +0600

    Login Create NewToken Is DONE With Sanctum Is DONE 💚✔

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -33,6 +33,33 @@ class AuthController extends Controller
         return response($response, 201);
     }
 
+    public function login(Request $request)
+    {
+        $fields = $request->validate([
+            'email' => 'required|string',
+            'password' => 'required|string',
+        ]);
+
+        //Check Email ******************
+        $user = User::where('email', $fields['email'])->first();
+
+        //Password Check ******************
+        if (!$user || !Hash::check($fields['password'], $user->password)) {
+            return response([
+                'message' => "Password check failed",
+            ], 401);
+        }
+
+        $token = $user->createToken('myapptoken')->plainTextToken;
+
+        $response = [
+            'user' => $user,
+            'token' => $token
+        ];
+
+        return response($response, 201);
+    }
+
     public function logout(Request $request)
     {
         auth()->user()->tokens()->delete();
--- routes/api.php
+++ routes/api.php
@@ -20,6 +20,7 @@ use Illuminate\Support\Facades\Route;
 //Public Routes
 //Route::resource('/products', ProductController::class);
 Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
 Route::get('/products', [ProductController::class, 'index']);
 Route::get('/products/{id}', [ProductController::class, 'show']);
 Route::get('products/search/{name}', [ProductController::class, 'search']);
